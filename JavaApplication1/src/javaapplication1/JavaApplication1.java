672168738528b46a9d4d013710c88714ba0694b7
Nicolas Tomas <nicolas.alarcon.l@usach.cl>
2017-08-25 23:11:59 -0300

    c
    c

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+
+/**
+ *
+ * @author Nico
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+           Vistas.Ventas.Agregar.Panel_Venta ventaNew = new Vistas.Ventas.Agregar.Panel_Venta();
+           ventaNew.setVisible(true);
+                 //  Vistas.Ventas.Buscar.Panel_Venta_Buscar  buscar = new Vistas.Ventas.Buscar.Panel_Venta_Buscar();
+               //buscar.setVisible(true);
+    }
+    
+}
